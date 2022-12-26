1784f479e266fd526bd0cb5cd595e53947b32228
Sajeed <saj.ans.rt19@rait.ac.in>
2022-12-26 19:33:46 +0530

    first commit

---
+++ admin/add-admin.php
@@ -0,0 +1,48 @@
+<?php
+include ('partials/menu.php');
+?>
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Add Admin</h1>
+        <br/><br/>
+        <form action="" method = "POST" >
+
+        <table class="tbl-50">
+            <tr>
+                <td>Full Name: </td>
+                <td><input type="text" name="full_name" placeholder = "Enter Your Name" ></td>
+            </tr>
+            <tr>
+                <td>Username</td>
+                <td>
+                    <input type="text" name= "username" placeholder = "Your Username">
+                </td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td>
+                    <input type="password" name="password" placeholder = "Your Password">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" name = "submit" value="Add Admin" class="btn-secondary">
+                </td>
+            </tr>
+        </table>
+
+        </form>
+
+    </div>
+</div>
+
+<?php
+include ('partials/footer.php');
+?>
+
+<?php
+// Process the value from form and save it in databases
+// Check wheter the submit button is click or not
+
+
+?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer Section Starts -->
+ <div class="footer">
+       <div class="wrapper">
+       <p class= "text-center">2020 All rights reserved, Food Destination. Developed by <a href="#">Sajeed Ansari</a> </p>
+       </div>
+    </div>
+         <!-- Footer Section End -->
+</body>
+</html>
